d5d81090dc513e1dc7900d58437e28478237a39b
Espin16 <54683735+Espin16@users.noreply.github.com>
2026-08-14 19:12:22 +0100

    Tidied up layout and useless subroutines in pose_detector

--- python/pose_detector.py
+++ python/pose_detector.py
@@ -45,22 +45,9 @@ def draw_landmarks_on_image(rgb_image: np.ndarray, detection_result: vision.Pose
 
 # Initialise objects
 
-BaseOptions = mp.tasks.BaseOptions
-PoseLandmarker = mp.tasks.vision.PoseLandmarker
-PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
-VisionRunningMode = mp.tasks.vision.RunningMode
-
-options = PoseLandmarkerOptions(base_options=BaseOptions(model_asset_path=MODEL_PATH),
-                                running_mode=VisionRunningMode.VIDEO,
-                                min_pose_detection_confidence=0.7,
-                                min_pose_presence_confidence=0.7,
-                                min_tracking_confidence=0.7)
-
-
-
 class PoseLandmarkerWrapper:
 
-    def __init__(self, model_path: str = MODEL_PATH, min_pose_detection_confidence: float = 0.7, min_pose_presence_confidence: float = 0.7, min_tracking_confidence: float = 0.7):
+    def __init__(self, model_path: str = MODEL_PATH, min_pose_detection_confidence: float = 0.8, min_pose_presence_confidence: float = 0.7, min_tracking_confidence: float = 0.8):
 
         base_options = mp.tasks.BaseOptions(model_asset_path = model_path)
         options = mp.tasks.vision.PoseLandmarkerOptions(base_options = base_options,
@@ -83,67 +70,3 @@ class PoseLandmarkerWrapper:
 
     def __exit__(self):
         self.close()
-
-
-
-
-
-
-# with PoseLandmarker.create_from_options(options) as landmarker:
-
-#     def main():
-
-#         capture = cv2.VideoCapture(0)
-#         timestamp_ms = 0
-#         frame_counter = 0
-
-
-#         if not capture.isOpened():
-#             print("Error: Could not open webcam.")
-#             return
-
-#         print("Press 'q' to quit the webcam feed.")
-
-#         time.sleep(2)
-
-#         while True:
-#             ret, frame = capture.read()
-#             timestamp_ms += frame_ms
-#             frame_counter += 1
-
-#             if not ret:
-#                 print("Error: Could not read frame.")
-#                 break
-
-#             # rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-#             mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
-#             pose_landmark_result = landmarker.detect_for_video(mp_image, int(timestamp_ms))
-
-#             image_copy = np.copy(mp_image.numpy_view())
-
-#             annotated_image = draw_landmarks_on_image(image_copy, pose_landmark_result)
-
-#             # if pose_landmark_result.segmentation_masks:
-#             #     segmentation_mask = pose_landmark_result.segmentation_masks[0].numpy_view()
-#             #     segmentation_mask = np.squeeze(segmentation_mask)
-
-#             #     visualized_mask = (segmentation_mask * 255).astype(np.uint8)
-#             #     visualized_mask = np.stack([visualized_mask]*3, axis=-1)
-
-#             if annotated_image is not None:
-#                 cv2.imshow('Webcam Feed', annotated_image)
-#             else:
-#                 cv2.imshow('Webcam Feed', frame)
-
-
-
-
-#             if cv2.waitKey(1) & 0xFF == ord('q'):
-#                 break
-
-#         capture.release()
-#         cv2.destroyAllWindows()
-
-#     main()
-
-    
